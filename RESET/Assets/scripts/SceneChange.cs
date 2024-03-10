956aa23a6b3c92e10a11f26914aca8a94a7ac8b5
GHong6 <gachong@ucsc.edu>
2024-03-10 01:03:51 -0800

    Fixed levels and URP
    Fixed all the Levels mesh
    added Universal shading
    added scene change collider

---
+++ RESET/Assets/scripts/SceneChange.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
